9123d1ce6bdd067a002dc415a49923ea79daa585
wwdu <wwdu@umich.edu>
2026-05-27 22:34:37 +0800

    Temporarily adding back signup feature

--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -25,8 +25,20 @@ module.exports = {
   },
 
   Mutation: {
-    register: async () => {
-      throw new Error('Registration is currently disabled. Please contact the administrator.');
+    register: async (_, { input }, { res }) => {
+      safetyService.validateRegisterInput(input);
+      const { token, user } = await authService.register(input.email, input.password);
+
+      if (res) {
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: env.NODE_ENV === 'production',
+          sameSite: 'strict',
+          maxAge: 24 * 60 * 60 * 1000 // 1 day
+        });
+      }
+
+      return { token, user };
     },
 
     login: async (_, { input }, { res }) => {
